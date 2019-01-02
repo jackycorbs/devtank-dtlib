cba15640fe8a2d62c07a562d0ab563b57d7e5864
Joe Burmeister <joe.burmeister@devtank.co.uk>
2019-01-02 11:43:50 +0000

    Add group list and run+ran views.

--- apps/example/example_lib_gui/__init__.py
+++ apps/example/example_lib_gui/__init__.py
@@ -1,20 +1,14 @@
 from context import gui_context_object
 
 from start_gui import open_start_page, init_start_page
+from group_run_gui import init_run_group, open_ran_group, open_run_group
+from groups_list_gui import init_groups_list, open_groups_list
 
-from dt_gui_base import base_groups_list_gui, init_query_gui
-
-_groups_singleton = None
-
-
-def open_groups_list(context):
-    context.push_view()
-    context.change_view("TestGroupActSelectorObj")
+from dt_gui_base import init_query_gui
 
 
 def init(context):
-    global _groups_singleton
     init_start_page(context)
-    _groups_singleton = base_groups_list_gui(context)
-
     init_query_gui(context)
+    init_run_group(context)
+    init_groups_list(context)
---
+++ apps/example/example_lib_gui/groups_list_gui.py
@@ -0,0 +1,30 @@
+from dt_gui_base import base_groups_list_gui
+
+_singleton = None
+
+class groups_lists_gui(base_groups_list_gui):
+    def __init__(self, context):
+        base_groups_list_gui.__init__(self, context)
+
+    def show_view(self):
+        base_groups_list_gui.show_view(self)
+        self.adv_more_btn.hide()
+
+    def _on_run_btn(self):
+        from group_run_gui import open_run_group
+        self._use_selection()
+        open_run_group(self.context)
+
+    def _results_btn(self):
+        from sessions_results_gui import open_tests_sessions_results
+        self._use_selection()
+        open_tests_sessions_results(self.context)
+
+
+def open_groups_list(context):
+    context.push_view()
+    context.change_view("TestGroupActSelectorObj")
+
+
+def init_groups_list(context):
+    _singleton = groups_lists_gui(context)
