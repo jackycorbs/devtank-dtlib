0b29d5bebe1098bac95d700b7ade19fef9cdc72f
Joe Burmeister <joe.burmeister@devtank.co.uk>
2019-01-25 15:45:25 +0000

    Remove old time setting on resources builder.

--- pylibapps/resource_builder.py
+++ pylibapps/resource_builder.py
@@ -2,8 +2,6 @@
 
 import os
 import sys
-import time
-import re
 
 if len(sys.argv) < 3:
     sys.stderr.write("Requires <out>.py <in> [<in>] ....\n")
@@ -17,8 +15,6 @@ out = open(sys.argv[1], "w")
 
 out.write("resources = {\n")
 
-st_mtime = os.stat(sys.argv[0]).st_mtime
-
 for in_filename in sys.argv[2:]:
     with open(in_filename) as in_file:
         data = in_file.read()
@@ -26,11 +22,7 @@ for in_filename in sys.argv[2:]:
         for b in data:
             out.write("\\x%02x" % ord(b))
         out.write('",\n')
-    stat = os.stat(in_filename)
-    if stat.st_mtime > st_mtime:
-        st_mtime = stat.st_mtime
 
 out.write("}")
 out.close()
 
-os.utime(sys.argv[1], (time.time(), st_mtime+1))
