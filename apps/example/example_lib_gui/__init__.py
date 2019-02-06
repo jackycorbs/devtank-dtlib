e889be64256a84d4ec3313b9e89ada218d55be88
Joe Burmeister <joe.burmeister@devtank.co.uk>
2019-02-06 20:40:26 +0000

    Fix example init and avoid same mistake repeating.

--- apps/example/example_lib_gui/__init__.py
+++ apps/example/example_lib_gui/__init__.py
@@ -7,20 +7,14 @@ from sessions_results_gui import init_tests_sessions_results,     \
                                  open_dev_tests_sessions_results, \
                                  open_tests_sessions_results
 
-from dt_gui_base import init_query_gui,           \
-                        init_test_group_selector, \
-                        init_tests_properties,    \
-                        init_test_group_naming,   \
+from dt_gui_base import dt_gui_base_init, \
                         open_test_group_selector, \
                         open_tests_properties,    \
                         open_test_group_naming
 
 def init(context):
+    dt_gui_base_init(context)
     init_start_page(context)
-    init_query_gui(context)
     init_run_group(context)
     init_groups_list(context)
-    init_test_group_selector(context)
-    init_tests_properties(context)
-    init_test_group_naming(context)
     init_tests_sessions_results(context)
--- pylibapps/dt_gui_base/__init__.py
+++ pylibapps/dt_gui_base/__init__.py
@@ -4,24 +4,31 @@ from common import populate_test_properties, get_pass_fail_icon_name, update_def
 from context import gui_context_object
 from find_or_create_gui import scan_find_or_create
 from new_gui import new_obj_view
-from notify_gui import init_notify_gui, open_notify_gui
-from query_gui import init_query_gui, open_query_gui
+from notify_gui import open_notify_gui
+from query_gui import open_query_gui
 
-from group_naming_gui import open_test_group_naming, init_test_group_naming
+from group_naming_gui import open_test_group_naming
 
-from edit_test_gui import open_edit_test_page, init_edit_test_page
-from new_test_gui import open_new_test_page, init_new_test_page
+from edit_test_gui import open_edit_test_page
+from new_test_gui import open_new_test_page
 
 from sessions_results_gui import base_session_results_singlton
 
-from group_selector_gui import open_test_group_selector, init_test_group_selector
-from group_properties_gui import open_tests_properties, init_tests_properties
+from group_selector_gui import open_test_group_selector
+from group_properties_gui import open_tests_properties
 
 from sessions_results_gui import base_session_results_singlton
 from groups_list_gui import base_groups_list_gui
 
 
 def dt_gui_base_init(context):
+    from notify_gui import init_notify_gui
+    from query_gui import init_query_gui
+    from group_selector_gui import init_test_group_selector
+    from group_properties_gui import init_tests_properties
+    from edit_test_gui import init_edit_test_page
+    from new_test_gui import init_new_test_page
+    from group_naming_gui import init_test_group_naming
     init_notify_gui(context)
     init_query_gui(context)
     init_test_group_selector(context)
