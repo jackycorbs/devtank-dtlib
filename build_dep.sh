ca62a842716d2cee23fcaa2a6995b90d243bf341
Joe Burmeister <joe.burmeister@devtank.co.uk>
2019-07-18 11:33:47 +0100

    Add script to pull down build dependencies on Debian based system.

---
+++ build_dep.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+sudo apt-get install gcc make pkg-config libyaml-dev libiio-dev python \
+    gir1.2-gtk-3.0 libgtk-3-0 \
+    python-yaml python-paramiko python-mysql.connector python-gi python-tz python-netifaces \
+    python3 python3-paramiko python3-mysql.connector
