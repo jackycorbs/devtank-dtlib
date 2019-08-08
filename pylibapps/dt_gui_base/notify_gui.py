e4ff502676524ff76ecb907c017edc5044a9cb28
Joe Burmeister <joe.burmeister@devtank.co.uk>
2019-08-08 11:39:52 +0100

    Add option to change icon of notify to cancel instead of ok.

--- pylibapps/dt_gui_base/notify_gui.py
+++ pylibapps/dt_gui_base/notify_gui.py
@@ -3,9 +3,11 @@ def _response_click(context):
     context.pop_view()
 
 
-def open_notify_gui(context, msg):
+def open_notify_gui(context, msg, cancel_btn=False):
     context.push_view()
     context.change_view("UserNotify")
+    notift_ok_btn = context.builder.get_object("notift_ok_btn")
+    notift_ok_btn.set_label("gtk-cancel" if cancel_btn else "gtk-ok")
     notify_lab = context.builder.get_object("notify_lab")
     notify_lab.set_text(msg)
 
