8a26991bbf7196355b88ad41c0f47b4830a4f84a
Joe Burmeister <joe.burmeister@devtank.co.uk>
2019-09-05 14:45:35 +0100

    Add optional callback on exit to notify page.

--- pylibapps/dt_gui_base/notify_gui.py
+++ pylibapps/dt_gui_base/notify_gui.py
@@ -1,15 +1,21 @@
 
+_exit_cb = None
+
 def _response_click(context):
     context.pop_view()
+    if _exit_cb:
+        _exit_cb()
 
 
-def open_notify_gui(context, msg, cancel_btn=False):
+def open_notify_gui(context, msg, cancel_btn=False, exit_cb=None):
     context.push_view()
     context.change_view("UserNotify")
     notift_ok_btn = context.builder.get_object("notift_ok_btn")
     notift_ok_btn.set_label("gtk-cancel" if cancel_btn else "gtk-ok")
     notify_lab = context.builder.get_object("notify_lab")
     notify_lab.set_text(msg)
+    global _exit_cb
+    _exit_cb = exit_cb
 
 
 def init_notify_gui(context):
