9bc74470d420343af999bc7b75b611ba92504c0d
Joe Burmeister <joe.burmeister@devtank.co.uk>
2020-10-06 22:56:25 +0100

    Py3 fixes.

--- pylibapps/dt_gui_base/common.py
+++ pylibapps/dt_gui_base/common.py
@@ -1,4 +1,4 @@
-from dt_db_base import get_test_doc, get_args_in_src, get_float_prop_digits
+from dt_db_base import get_test_doc, get_args_in_src, get_float_prop_digits, dbfile
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -299,7 +299,7 @@ def _set_property_type_gui(context, prop):
         cell = Gtk.CellRendererText()
         files_drop.pack_start(cell, True)
         files_drop.add_attribute(cell, "text", 0)
-        all_file_ids = all_files.keys()
+        all_file_ids = list(all_files.keys())
         for file_id in all_file_ids:
             filename = all_files[file_id]
             key = "%i:%s" % (file_id, filename)
