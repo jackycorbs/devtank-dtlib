dd1a05a18544eb4401063ef27caf0b40ed965f4c
Joe Burmeister <joe.burmeister@devtank.co.uk>
2021-05-12 13:16:07 +0100

    Use multiple inheritance for an easy life.

--- apps/gui/example_lib_gui/context.py
+++ apps/gui/example_lib_gui/context.py
@@ -3,21 +3,10 @@ import example_lib
 from dt_gui_base import gui_context_object as base_gui_context_object
 
 
-class gui_context_object(base_gui_context_object):
+class gui_context_object(base_gui_context_object, example_lib.cli_context_object):
     def __init__(self, args, db_def, builder):
+        example_lib.cli_context_object.__init__(self, args, db_def)
         base_gui_context_object.__init__(self, args, db_def, builder)
-        self.__bus = example_lib.example_bus()
-        self.locked = False
-        self.db_dev = None
-
-    def lock_bus(self):
-        if self.locked:
-            return None
-        self.locked = True
-        return self.__bus
-
-    def release_bus(self):
-        self.locked = False
 
     def _db_fail(self, e):
         base_gui_context_object._db_fail(self, e)
