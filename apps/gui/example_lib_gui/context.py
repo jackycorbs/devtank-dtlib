02da08f45d9ade2fa91a993a4924f99769bff369
Joe Burmeister <joe.burmeister@devtank.co.uk>
2021-05-19 17:00:24 +0100

    Name correction since resource files change.

--- apps/gui/example_lib_gui/context.py
+++ apps/gui/example_lib_gui/context.py
@@ -4,9 +4,9 @@ from dt_gui_base import gui_context_object as base_gui_context_object
 
 
 class gui_context_object(base_gui_context_object, example_lib.cli_context_object):
-    def __init__(self, args, db_def, builder):
+    def __init__(self, args, db_def, glade_files):
         example_lib.cli_context_object.__init__(self, args, db_def)
-        base_gui_context_object.__init__(self, args, db_def, builder)
+        base_gui_context_object.__init__(self, args, db_def, glade_files)
 
     def _db_fail(self, e):
         base_gui_context_object._db_fail(self, e)
