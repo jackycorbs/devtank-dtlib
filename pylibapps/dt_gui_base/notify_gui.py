1bea6984aba6c5524a3ab1344afbd1e068fa51ef
Joe Burmeister <joe.burmeister@devtank.co.uk>
2021-05-20 22:48:49 +0100

    Restructure notification to class.

--- pylibapps/dt_gui_base/notify_gui.py
+++ pylibapps/dt_gui_base/notify_gui.py
@@ -1,24 +1,35 @@
 
-_exit_cb = None
 
-def _response_click(context):
-    context.pop_view()
-    if _exit_cb:
-        _exit_cb()
+_singleton = None
+
+
+class notify_object(object):
+    def __init__(self, context):
+        self.context = context
+        self._exit_cb = None
+        self.notift_ok_btn = context.builder.get_object("notift_ok_btn")
+        self.notify_lab = context.builder.get_object("notify_lab")
+        self.notift_ok_btn.connect("clicked", lambda x: self._response_click())
+
+    def _response_click(self):
+        self.context.pop_view()
+        if self._exit_cb:
+            self._exit_cb()
+
+    def open(self, msg, cancel_btn=False, exit_cb=None):
+        context = self.context
+        context.push_view()
+        context.change_view("UserNotify")
+        self.notift_ok_btn.set_label("gtk-cancel" if cancel_btn else "gtk-ok")
+        self.notify_lab.set_text(msg)
+        self._exit_cb = exit_cb
+
 
 
 def open_notify_gui(context, msg, cancel_btn=False, exit_cb=None):
-    context.push_view()
-    context.change_view("UserNotify")
-    notift_ok_btn = context.builder.get_object("notift_ok_btn")
-    notift_ok_btn.set_label("gtk-cancel" if cancel_btn else "gtk-ok")
-    notify_lab = context.builder.get_object("notify_lab")
-    notify_lab.set_text(msg)
-    global _exit_cb
-    _exit_cb = exit_cb
+    _singleton.open(msg, cancel_btn, exit_cb)
 
 
 def init_notify_gui(context):
-    builder = context.builder
-    notift_ok_btn = builder.get_object("notift_ok_btn")
-    notift_ok_btn.connect("clicked", lambda x: _response_click(context))
+    global _singleton
+    _singleton = notify_object(context)
