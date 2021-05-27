cf4111a14ad81a0354297a760d39e6ba0e4ab847
Joe Burmeister <joe.burmeister@devtank.co.uk>
2021-05-27 09:24:59 +0100

    Drop run script from bash to sh.

--- create_run_script.sh
+++ create_run_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "#! /bin/bash" > $2
+echo "#! /bin/sh" > $2
 echo 'own_dir="$(dirname $(readlink -f $0))"' >> $2
 echo 'plat_lib="$own_dir/../lib/$(getconf LONG_BIT)"' >> $2
 echo 'if [ ! -e "$plat_lib" ]; then' >> $2
