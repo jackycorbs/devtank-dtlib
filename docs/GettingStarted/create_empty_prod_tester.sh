f8be913ec5c70687ec4f43bfd3bddba8ca3bd355
Joe Burmeister <joe.burmeister@devtank.co.uk>
2022-10-27 10:16:59 +0100

    Pathing tweaks to empty project creator script.

--- docs/GettingStarted/create_empty_prod_tester.sh
+++ docs/GettingStarted/create_empty_prod_tester.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 if [[ $# -ne 1 ]] ; then
     echo './create_empty_prod_tester.sh [project_name]'
     exit 0
@@ -7,10 +10,11 @@ fi
 PRO_NAME=$(basename "$1")
 echo Project name = $PRO_NAME
 
-SUBM=$(git remote get-url origin)
+own_dir=$(dirname "$0")
+dtlib_dir=$(readlink -f "$own_dir"/../..)
+SUBM=$(git --git-dir="$dtlib_dir"/.git remote get-url origin)
 echo Submodule - $SUBM
-CWD=$(pwd)
-echo current working directory = $CWD
+echo dtlib directory = $dtlib_dir
 
 {
     mkdir -p "$1"
@@ -19,22 +23,21 @@ echo current working directory = $CWD
 }
 git init "$1"
 cd "$1"
-echo Full path = $1
+echo Full path = $(readlink -f "$1")
 
 git submodule add $SUBM dtlib
 git commit -m "Added dtlib submodule to repository."
 mkdir apps
-cd $CWD
 
 echo About to copy template and cli to apps
-cp -R ../../apps/template_gui "$1"/apps
-cp -R ../../apps/template_cli "$1"/apps
+cp -R "$dtlib_dir"/apps/template_gui apps
+cp -R "$dtlib_dir"/apps/template_cli apps
 echo $about to move $1/apps/template_gui to $1/apps/"$PRO_NAME"_gui
 
-mv "$1"/apps/template_gui "$1"/apps/"$PRO_NAME"_gui
-mv "$1"/apps/template_cli "$1"/apps/"$PRO_NAME"_cli
-cd "$1"
-./dtlib/docs/GettingStarted/rename.sh $PRO_NAME apps/
+mv apps/template_gui apps/"$PRO_NAME"_gui
+mv apps/template_cli apps/"$PRO_NAME"_cli
+
+"$dtlib_dir"/docs/GettingStarted/rename.sh $PRO_NAME apps/
 cd apps/"$PRO_NAME"_cli
 rm -r example_lib resources tests
 ln -s ../"$PRO_NAME"_gui/"$PRO_NAME"_lib
