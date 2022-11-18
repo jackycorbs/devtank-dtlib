864ac0da2c5cdb8423affdad31b2f3db741601d2
Nathan Weetman <nathan.weetman@devtank.co.uk>
2022-11-18 14:49:15 +0000

    Move buttons on each click to avoid touchscreen double-presses

--- pylibapps/dt_gui_base/query_gui.py
+++ pylibapps/dt_gui_base/query_gui.py
@@ -1,23 +1,40 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 class _user_query_singleton(object):
     def __init__(self, context):
         self.return_cb = None
         self.msg = None
         self.context = context
-        builder = context.builder
-        self.query_lab = builder.get_object("query_lab")
-        query_yes_btn = builder.get_object("query_yes_btn")
-        query_no_btn  = builder.get_object("query_no_btn")
+        self.builder = context.builder
+        self.query_lab = self.builder.get_object("query_lab")
+        self.query_yes_btn_box = self.builder.get_object("query_yes_btn_box")
+        self.query_no_btn_box = self.builder.get_object("query_no_btn_box")
+        self.query_yes_btn = self.builder.get_object("query_yes_btn")
+        self.query_no_btn  = self.builder.get_object("query_no_btn")
 
-        query_yes_btn.connect("clicked", lambda x: self.response_click(True))
-        query_no_btn.connect("clicked",  lambda x: self.response_click(False))
+        self.query_yes_btn.connect("clicked", lambda x: self.response_click(True))
+        self.query_no_btn.connect("clicked",  lambda x: self.response_click(False))
 
     def do(self, msg, return_cb):
         self.msg = msg
         self.query_lab.set_text(msg)
         self.return_cb = return_cb
+        self.invert_button_positions()
         self.context.push_view()
         self.context.change_view("UserQuery")
+
+    def invert_button_positions(self):
+        current_pack_type = self.query_yes_btn_box.query_child_packing(self.query_yes_btn)[3]
+        if current_pack_type == Gtk.PackType.START:
+            target_pack_type = Gtk.PackType.END
+        else:
+            target_pack_type = Gtk.PackType.START
+
+        self.query_yes_btn_box.set_child_packing(self.query_yes_btn, False, True, 30, target_pack_type)
+        self.query_no_btn_box.set_child_packing(self.query_no_btn, False, True, 30, target_pack_type)
+
 
     def response_click(self, yes_no):
         self.context.pop_view()
