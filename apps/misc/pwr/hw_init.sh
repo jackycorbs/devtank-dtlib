e27ce9af0e92e873f489d0b2772073519443a760
Joe Burmeister <joe.burmeister@devtank.co.uk>
2023-06-16 10:27:39 +0100

    Add work round to udev/init bug with pwr_dac symlink setup.

--- apps/misc/pwr/hw_init.sh
+++ apps/misc/pwr/hw_init.sh
@@ -16,4 +16,17 @@ echo 1 > /sys/bus/iio/devices/$pwr_adc/in_voltage1_scale
 echo 1 > /sys/bus/iio/devices/$pwr_adc/in_voltage2_scale
 echo 1 > /sys/bus/iio/devices/$pwr_adc/in_voltage3_scale
 
+if [ ! -e /dev/pwr_dac ]
+then
+  echo "No /dev/pwr_dac (udev bug?)"
+  of_name=$(grep -al pwr_dac /sys/bus/iio/devices/*/of_node/name)
+  if [ -n "$of_name" ]
+  then
+    iio_name=$(basename $(dirname $(dirname $of_name)))
+    iio_index=${iio_name:10}
+    iio_major=$(grep iio /proc/devices | awk '{print $1}')
+    mknod /dev/$iio_name c $iio_major $iio_index
+    ln -s $iio_name /dev/pwr_dac
+  fi
+fi
 
