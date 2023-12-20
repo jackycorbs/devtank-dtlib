0b5863ce0f2907daf000c33d6a30aed8131be595
Harry Geyer <harry.geyer@devtank.co.uk>
2023-12-20 10:51:57 +0000

    Remove logging for use by tests. And cleanup some debug.

--- pylibapps/dt_db_base/py_base.py
+++ pylibapps/dt_db_base/py_base.py
@@ -3,92 +3,12 @@ import sys
 import atexit
 import time
 import datetime
-import logging
 import subprocess
-
-""" This is to toggle the use of the python logging library """
-__USE_LOGGING__ = False
 
 
 _ANSI_RED     = "\x1B[31m"
 _ANSI_GREEN   = "\x1B[32m"
 _ANSI_DEFAULT = "\x1B[39m"
-
-class _logging_formatter_t(logging.Formatter):
-    RED         = "\033[31;20m"
-    GREEN       = "\033[32;20m"
-    YELLOW      = "\033[33;20m"
-    BLUE        = "\033[34;20m"
-    MAGENTA     = "\033[35;20m"
-    CYAN        = "\033[36;20m"
-    GREY        = "\033[37;20m"
-    WHITE       = "\033[39:20m"
-    BOLD_RED    = "\033[31;1m"
-    RESET       = WHITE
-    FORMAT      = "%(asctime)s.%(msecs)03d [%(thread)s] %(levelname)s: %(message)s"
-    DATEFMT     = "%d/%m %T"
-
-    FORMATS = {
-        logging.DEBUG:    GREY     + FORMAT + RESET,
-        logging.INFO:     WHITE    + FORMAT + RESET,
-        logging.WARNING:  YELLOW   + FORMAT + RESET,
-        logging.ERROR:    RED      + FORMAT + RESET,
-        logging.CRITICAL: BOLD_RED + FORMAT + RESET
-    }
-
-    def _format_colour(self, record):
-        #log_fmt = self.FORMATS.get(record.levelno)
-        #formatter = logging.Formatter(log_fmt)
-        formatter = super(self.FORMAT)
-        formatter.datefmt   = "%d/%m %T"
-        formatter.converter = time.gmtime
-        return formatter.format(record)
-
-    def _format_no_colour(self, record):
-        formatter = logging.Formatter(self.FORMAT)
-        formatter.datefmt   = "%d/%m %T"
-        formatter.converter = time.gmtime
-        return formatter.format(record)
-
-    def colour(self, enabled):
-        if enabled:
-            self.format = self._format_colour
-        else:
-            self.format = self._format_no_colour
-
-class _logging_stream_handler_less_than_filter(logging.Filter):
-    def __init__(self, exclusive_maximum, name=""):
-        super(_logging_stream_handler_less_than_filter, self).__init__(name)
-        self.max_level = exclusive_maximum
-    def filter(self, record):
-        return 1 if record.levelno < self.max_level else 0
-
-#global logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.NOTSET)
-
-_formatter = _logging_formatter_t()
-is_a_tty = os.isatty(sys.stdout.fileno())
-
-_formatter.colour(is_a_tty)
-
-if is_a_tty:
-    _stdout_handler = logging.StreamHandler(sys.stdout)
-    _stdout_handler.setLevel(logging.INFO)
-    _stdout_handler.addFilter(_logging_stream_handler_less_than_filter(logging.WARNING))
-    _stdout_handler.setFormatter(_formatter)
-
-    _stderr_handler = logging.StreamHandler(sys.stderr)
-    _stderr_handler.setLevel(logging.WARNING)
-    _stderr_handler.setFormatter(_formatter)
-    logger.addHandler(_stdout_handler)
-    logger.addHandler(_stderr_handler)
-
-else:
-    _file_handler = logging.FileHandler() # TODO: This needs to have the log file location in I think...
-    _file_handler.setLevel(logging.INFO)
-    _file_handler.addFormatter(_formatter)
-    logger.addHandler(_file_handler)
 
 # No C file, so use python functions
 
@@ -96,15 +16,23 @@ dt_usecs = int
 
 get_current_us = lambda: dt_usecs(time.time_ns / 1000.)
 
-_devtank_init       = lambda: None
-_devtank_ready      = lambda: True
-_devtank_shutdown   = lambda: None
+_devtank_is_ready = False
+
+def _devtank_init():
+    global _devtank_is_ready
+    _devtank_is_ready = True
+
+def _devtank_ready():
+    global _devtank_is_ready
+    return _devtank_is_ready
+
+def _devtank_shutdown():
+    global _devtank_is_ready
+    _devtank_is_ready = False
 
 def _set_log_fd(fd):
     global log_fd
-    print(f"SETTING {fd = }")
     log_fd = fd
-    print(f"{log_fd = }")
 
 def _get_log_fd():
     global log_fd
@@ -119,25 +47,17 @@ def _log_msg(fd: int, msg: str, colour: str = _ANSI_DEFAULT):
         pl = f"{datetime.datetime.now().strftime('%d/%m %T.%f')} [{os.getpid()}] {msg}\n"
     return os.write(fd, pl.encode())
 
-if __USE_LOGGING__:
-    error_msg           = lambda *x: logger.error(*x)
-    warning_msg         = lambda *x: logger.warning(*x)
-    info_msg            = lambda *x: logger.info(*x)
-    enable_info_msgs    = lambda x: logger.setLevel(logging.INFO) if x else logger.setLevel(logging.ERROR)
-    enable_warning_msgs = lambda x: logger.setLevel(logging.WARNING) if x else logger.setLevel(logging.ERROR)
-    info_msgs_is_enabled = lambda: logger.level <= logging.INFO
-else:
-    _error_msg          = lambda *x: _log_msg(_get_log_fd(), f"ERROR: {str(*x)}", colour=_ANSI_RED)
-    _warning_msg        = lambda *x: _log_msg(_get_log_fd(), f"WARNING: {str(*x)}", colour=_ANSI_RED)
-    _info_msg           = lambda *x: _log_msg(_get_log_fd(), f"INFO: {str(*x)}")
-    error_msg           = _error_msg
-    warning_msg         = _warning_msg
-    info_msg            = _info_msg
-    def enable_info_msgs(enable: bool):
-        info_msg = _info_msg if enable else lambda *x: None
-    def enable_warning_msgs(enable: bool):
-        warning_msg = _warning_msg if enable else lambda *x: None
-    info_msgs_is_enabled = lambda: info_msg is _info_msg
+_error_msg          = lambda *x: _log_msg(_get_log_fd(), f"ERROR: {str(*x)}", colour=_ANSI_RED)
+_warning_msg        = lambda *x: _log_msg(_get_log_fd(), f"WARNING: {str(*x)}", colour=_ANSI_RED)
+_info_msg           = lambda *x: _log_msg(_get_log_fd(), f"INFO: {str(*x)}")
+error_msg           = _error_msg
+warning_msg         = _warning_msg
+info_msg            = _info_msg
+def enable_info_msgs(enable: bool):
+    info_msg = _info_msg if enable else lambda *x: None
+def enable_warning_msgs(enable: bool):
+    warning_msg = _warning_msg if enable else lambda *x: None
+info_msgs_is_enabled = lambda: info_msg is _info_msg
 
 def dt_get_build_info():
     cmd_get_commit = "git log -n 1 --format=\"%h-%f\""
